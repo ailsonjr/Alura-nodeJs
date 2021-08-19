631c081ae203e49938611833cb844baec525a10f
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-08-19 13:46:27 -0300

    Manipulando a imagem no servidor

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
-fs.createReadStream('./assets/maya.jpg')
-  .pipe(fs.createWriteStream('./assets/maya-stream.jpg'))
-  .on('finish', () => console.log('Imagem foi escrita com sucesso'));
+module.exports = (caminho, nomeDoArquivo, callbaclImagemCriada) => {
+  const novoCaminho = `./assets/images/${nomeDoArquivo}.jpg`;
+
+  fs.createReadStream(caminho)
+    .pipe(fs.createWriteStream(novoCaminho))
+    .on('finish', () => callbaclImagemCriada(novoCaminho));
+};
